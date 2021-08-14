0cdc1c76e4252a8cb248f222f39151839a035759
Robert Haase <haesleinhuepf@users.noreply.github.com>
2021-08-14 13:06:26 +0200

    initial version

---
+++ lut.ijm
@@ -0,0 +1,2 @@
+newImage("Untitled", "8-bit ramp", 256, 20, 1);
+run("Red Hot");
---
+++ utils.py
@@ -0,0 +1,3 @@
+from pyclesperanto_prototype import imshow
+def show(image, min_display_intensity=0, max_display_intensity=1):
+    imshow(image, min_display_intensity=min_display_intensity, max_display_intensity=max_display_intensity)
